e32b6f8eed37d67f412c1b7738c6dec9df506570
Maxwell <nanaforsonjnr@gmail.com>
2024-03-23 00:00:01 +0000

    Add a manifest that creates 'tmp/school'

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# This manifest creates a file 'school' in 'tmp'
+
+file {'/tmp/school':
+  ensure  => present,
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
