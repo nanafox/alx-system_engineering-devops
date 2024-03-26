87d26a05e6bcc33c1db0accfdca04163d96cd490
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-03-26 22:08:11 +0000

    Add Puppet manifest to install and configure Nginx web server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,68 @@
+# A manifest that configures an Nginx web server with the following:
+# - A root page that displays "Hello World!"
+# - A redirection page that redirects to https://lzcorp-landing-page.vercel.app/
+# - A 404 page that displays "Ceci n'est pas une page"
+$nginx_package_name = 'nginx'
+$nginx_service_name = 'nginx'
+$root_dir = '/usr/share/nginx/html'
+$config_file = '/etc/nginx/sites-available/default'
+$temp_config_file = '/tmp/new_config'
+$server_config = @(END)
+server_name _; 
+location /redirect_me { 
+  return 301 https://lzcorp-landing-page.vercel.app/; 
+} 
+error_page 404 /404.html; 
+location = /404.html { 
+  root /usr/share/nginx/html; 
+  internal; 
+}
+END
+
+exec { 'apt-get update':
+  command => '/usr/bin/apt-get update',
+  path    => ['/usr/bin', '/usr/sbin'],
+}
+
+package { $nginx_package_name:
+  ensure  => installed,
+  require => Exec['apt-get update'],
+}
+
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => "Hello World!\n",
+  require => Package[$nginx_package_name],
+}
+
+file { "${root_dir}/404.html":
+  ensure  => file,
+  content => "Ceci n'est pas une page\n",
+  require => Package[$nginx_package_name],
+}
+
+file { $temp_config_file:
+  ensure  => file,
+  content => $server_config,
+  require => Package[$nginx_package_name],
+}
+
+exec { 'update_nginx_config':
+  command => "sed -i '/server_name _;/{
+    r ${temp_config_file}
+  }' ${config_file}",
+  path    => ['/usr/bin', '/usr/sbin'],
+  require => File[$temp_config_file],
+}
+
+exec { 'remove-config-file':
+  command => "rm ${temp_config_file}",
+  path    => ['/usr/bin', '/usr/sbin'],
+  require => Exec['update_nginx_config'],
+}
+
+service { $nginx_service_name:
+  ensure  => running,
+  enable  => true,
+  require => Exec['update_nginx_config'],
+}
