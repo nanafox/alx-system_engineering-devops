c0685babfb73f6d3baf2559f6cc16e0e953abab7
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-04-24 22:27:45 +0000

    Refactor scripts to abstract the WHAT from the HOW

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,39 +4,8 @@
 This module provides a function to export user tasks to a CSV file.
 """
 
-import csv
+import lazy_methods
 import sys
-
-lazy_methods = __import__("0-gather_data_from_an_API")
-
-
-def export_to_csv(user_id: int, username: str, tasks: "list[dict]") -> None:
-    """
-    Export user tasks to a CSV file.
-
-    Args:
-        user_id (int): The ID of the user.
-        username (str): The username of the user.
-        tasks (list[dict]): A list of dictionaries representing the tasks.
-    """
-    with open(f"{user_id}.csv", "w", newline="") as csv_file:
-        fieldnames = [
-            "USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"
-        ]
-        writer = csv.DictWriter(
-            csv_file, fieldnames=fieldnames, quoting=csv.QUOTE_ALL
-        )
-
-        for task in tasks:
-            writer.writerow(
-                {
-                    "USER_ID": f"{user_id}",
-                    "USERNAME": f"{username}",
-                    "TASK_COMPLETED_STATUS": f"{task.get('completed')}",
-                    "TASK_TITLE": f"{task.get('title')}",
-                }
-            )
-
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -50,4 +19,4 @@ if __name__ == "__main__":
         sys.exit(1)
 
     todos = lazy_methods.get_todos(user_id)
-    export_to_csv(user_id=user_id, username=user, tasks=todos)
+    lazy_methods.export_to_csv(user_id=user_id, username=user, tasks=todos)
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -4,35 +4,8 @@
 This module provides a function to export user tasks to a JSON file.
 """
 
-import json
+import lazy_methods
 import sys
-
-lazy_methods = __import__("0-gather_data_from_an_API")
-
-
-def export_to_json(user_id: int, username: str, tasks: "list[dict]") -> None:
-    """
-    Export user tasks to a JSON file.
-
-    Args:
-        user_id (int): The ID of the user.
-        username (str): The username of the user.
-        tasks (list[dict]): A list of dictionaries representing the tasks.
-    """
-    data = {f"{user_id}": []}
-
-    for task in tasks:
-        task_data = {
-            "task": task.get("title"),
-            "completed": task.get("completed"),
-            "username": username,
-        }
-
-        data[user_id].append(task_data)
-
-    with open(f"{user_id}.json", "w", encoding="utf-8") as json_file:
-        json.dump(data, json_file, indent=4)
-
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -46,4 +19,4 @@ if __name__ == "__main__":
         sys.exit(1)
 
     todos = lazy_methods.get_todos(user_id)
-    export_to_json(user_id=user_id, username=user, tasks=todos)
+    lazy_methods.export_to_json(user_id=user_id, username=user, tasks=todos)
