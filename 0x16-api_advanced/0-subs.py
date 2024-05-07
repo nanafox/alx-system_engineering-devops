27f761a15864a0e333e117cd54a393e97b638863
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-05-07 13:51:12 +0000

    chore: Add script to retrieve number of subscribers for a subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This script queries the Reddit API and returns the number of total
+subscribers for a given subreddit."""
+
+import requests
+
+
+def number_of_subscribers(subreddit: str) -> int:
+    """
+    Retrieves the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers for the subreddit. Returns 0 if the
+        request fails or the subreddit does not exist.
+    """
+    response = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        allow_redirects=False,
+    )
+
+    return (
+        response.json()["data"]["subscribers"]
+        if response.status_code == 200
+        else 0
+    )
